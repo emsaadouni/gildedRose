71722277803569663f9fadefab2343aa02cf6061
emsaadouni <el-mahdi.saadouni@gmail.com>
2019-07-09 09:04:07 +0200

    First commit

---
+++ src/main/java/com/gildedrose/Item.java
@@ -0,0 +1,39 @@
+package com.gildedrose;
+
+import java.util.Objects;
+
+public class Item {
+
+    public String name;
+
+    public int sellIn;
+
+    public int quality;
+
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+   @Override
+   public String toString() {
+
+        return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Item)) return false;
+        Item item = (Item) o;
+        return sellIn == item.sellIn &&
+                quality == item.quality &&
+                Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, sellIn, quality);
+    }
+}
