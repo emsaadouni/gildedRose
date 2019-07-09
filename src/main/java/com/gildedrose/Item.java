a8f8bfb5e290ec8d29bda5a289bf5513c1fb9d24
emsaadouni <el-mahdi.saadouni@gmail.com>
2019-07-09 11:16:12 +0200

    Gilded Rose Approaval tests

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -10,30 +10,43 @@ public class Item {
 
     public int quality;
 
-    public Item(String name, int sellIn, int quality) {
+    Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
 
-   @Override
+    public static Item createItem(String name, int sellIn, int quality) {
+        switch (name) {
+            case "Aged Brie":
+                return new AgedBrie(sellIn, quality);
+            case "Backstage passes to a TAFKAL80ETC concert":
+                return new BackstagePass(sellIn, quality);
+            case "Sulfuras, Hand of Ragnaros":
+                return new Sulfuras(sellIn, quality);
+            default:
+                return new Item(name, sellIn, quality);
+        }
+
+
+    }
+
+    @Override
    public String toString() {
 
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Item)) return false;
-        Item item = (Item) o;
-        return sellIn == item.sellIn &&
-                quality == item.quality &&
-                Objects.equals(name, item.name);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, sellIn, quality);
+    protected void doUpdateQuality() {
+        if (quality > 0) {
+            quality = quality - 1;
+        }
+        sellIn = sellIn - 1;
+        if (sellIn < 0) {
+            if (quality > 0) {
+                quality = quality - 1;
+            }
+        }
     }
 }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,13 +10,13 @@ public class GildedRoseTest {
 
         CombinationApprovals.verifyAllCombinations(
                 this::doUpdateQuality,
-                new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+                new String[]{"Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
                 new Integer[]{-1, 0, 2, 6, 11},
                 new Integer[]{0, 1, 49, 50});
     }
 
     private String doUpdateQuality(String name, int sellIn, int quality) {
-        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        Item[] items = new Item[] {Item.createItem(name, sellIn, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         return app.items[0].toString();
